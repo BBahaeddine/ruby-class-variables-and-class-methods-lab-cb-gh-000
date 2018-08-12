9fa427565230216e29e5a6f5b4caa0715127df28
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-12 04:59:20 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    if @@genres.include?(genre) == false
+      @@genres << genre
+    end
+    if @@artists.include?(artist) == false
+      @@artists << artist
+    end
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+end
